28a580dc245cf91e665e56c157624c4cbffdfc98
Earl Hans Genoso <earlhansg@gmail.com>
2023-02-26 06:27:51 +0800

    fix conflicts

---
+++ src/main/java/ehg/base/WebDriverInstance.java
@@ -0,0 +1,66 @@
+package ehg.base;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class WebDriverInstance {
+
+	public static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+	public static WebDriver getDriver() {
+		if (driver.get() == null) {
+			try {
+				driver.set(createDriver());
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+		return driver.get();
+	}
+
+	public static WebDriver createDriver() throws IOException {
+		WebDriver driver = null;
+
+		Properties prop = new Properties();
+		FileInputStream data = new FileInputStream(
+				System.getProperty("user.dir") + "\\src\\main\\java\\ehg\\resources\\config.properties");
+		prop.load(data);
+
+		if (prop.getProperty("browser").equals("chrome")) {
+			System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\src\\main\\java\\ehg\\drivers\\chromedriver.exe");
+			ChromeOptions options = new ChromeOptions();
+			options.addArguments("start-maximized");
+			options.addArguments("disable-infobars");
+			options.addArguments("--disable-extensions");
+			driver = new ChromeDriver(options);
+		} else if (prop.getProperty("browser").equals("firefox")) {
+			System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\src\\main\\java\\ehg\\drivers\\geckodriver.exe");
+			driver = new FirefoxDriver();
+		} else {
+			System.setProperty("webdriver.chrome.driver",
+				System.getProperty("user.dir") + "\\src\\main\\java\\ehg\\drivers\\msedgedriver.exe");
+			driver = new EdgeDriver();
+		}
+
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+		return driver;
+	}
+
+	public static void cleanupDriver() {
+		driver.get().quit();
+		driver.remove();
+	}
+
+}
---
+++ src/main/java/ehg/pageObjects/LoginPage.java
@@ -0,0 +1,64 @@
+package ehg.pageObjects;
+
+import java.io.IOException;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import ehg.base.BasePage;
+
+public class LoginPage extends BasePage {
+
+    public WebDriver driver;
+
+    By emailAddressField = By.cssSelector("input#inputEmail");
+    By passwordField = By.cssSelector("input#inputPassword");
+    By notRobotCheckBox =  By.cssSelector("[data-phone-cc-input]");
+    By loginButton = By.cssSelector("button#login");
+    By imNotRobotChechBox = By.xpath("/html//span[@id='recaptcha-anchor']");
+    By rememberMeCheckBox = By.cssSelector("input[name='rememberme']");
+    By createAccountLink = By.linkText("Create account");
+
+
+    public LoginPage() throws IOException {
+        super();
+        //TODO Auto-generated constructor stub
+    }
+
+    public WebElement getEmailAddressField() throws IOException {
+        this.driver = getDriver();
+        return driver.findElement(emailAddressField);
+    }
+
+    public WebElement getPasswordField() throws IOException {
+        this.driver = getDriver();
+        return driver.findElement(passwordField);
+    }
+
+    public WebElement getNotRobotCheckBox() throws IOException {
+        this.driver = getDriver();
+        return driver.findElement(notRobotCheckBox);
+    }
+
+    public WebElement getLoginButton() throws IOException {
+        this.driver = getDriver();
+        return driver.findElement(loginButton);
+    }
+
+    public WebElement getRememberMeCheckBox() throws IOException {
+        this.driver = getDriver();
+        return driver.findElement(rememberMeCheckBox);
+    }
+
+    public WebElement getImNotRobotChechBox() throws IOException {
+        this.driver = getDriver();
+        return driver.findElement(imNotRobotChechBox);
+    }
+
+    public WebElement getCreateAccountLink() throws IOException {
+        this.driver = getDriver();
+        return driver.findElement(createAccountLink);
+    }
+    
+}
